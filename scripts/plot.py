99a75d2c7285573da0925f0598cd70e31b0383d4
Christopher Medlin <christopherjmedlin@mailbox.org>
2022-12-09 23:08:39 -0700

    fix I/O timings, make plots prettier

--- scripts/plot.py
+++ scripts/plot.py
@@ -19,6 +19,8 @@ def plot_timings(f, name):
         nums = re.findall("(\d+\.\d+)|(\d+)", line)
         time = nums[1][0]
         times.append(time)
+    plot.xlabel("n")
+    plot.ylabel("time/iteration")
     plot.plot(ns, list(map(float, times)), 
               '-o', label=name)
 
@@ -29,24 +31,29 @@ def plot_io(f, name):
     for line in ls[1::2]:
         nums = re.findall("\d+\.\d+", line)
         times.append(nums[0])
-    print(times)
+    plot.xlabel("n")
+    plot.ylabel("i/o time")
     plot.plot(ns, list(map(float, times)),
               '-o', label=name);
     
 if __name__ == "__main__":
     plot_timings(open("timings/64_grid.txt"), "grid")    
     plot_timings(open("timings/64_row.txt"), "row")
+    plot.legend()
     plot.savefig("coms64.png")
     plot.close()
     plot_io(open("timings/64_grid.txt"), "grid")
     plot_io(open("timings/64_row.txt"), "row")
+    plot.legend()
     plot.savefig("io64.png")
     plot.close()
     plot_timings(open("timings/16_grid.txt"), "grid")    
     plot_timings(open("timings/16_row.txt"), "row")
+    plot.legend()
     plot.savefig("coms16.png")
     plot.close()
     plot_io(open("timings/16_grid.txt"), "grid")
     plot_io(open("timings/16_row.txt"), "row")
+    plot.legend()
     plot.savefig("io16.png")
     plot.close()
